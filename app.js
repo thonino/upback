d99079e943d1484a8e44add28e81c0d4377bb2c8
thonino <thoninoben@gmail.com>
2023-10-05 20:20:34 +0200

    bug 1.2

--- app.js
+++ app.js
@@ -618,10 +618,10 @@ app.get("/payementsuccess/:invoiceId", async (req, res) => {
 });
 
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 server.listen(5000, () => {
