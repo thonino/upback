95df3963df74d29924980a0479d5abf6c0b5e5b0
thonino <thoninoben@gmail.com>
2023-10-09 21:49:09 +0200

    maj

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(helmet.contentSecurityPolicy({
 }));
 
 app.use(cors({ 
-  origin: 'https://uppercase-front-b016249fb8c8.herokuapp.com', 
+  origin: 'http://localhost:3000', 
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true
